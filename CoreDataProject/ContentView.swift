a3dd0befc5f7b7e586eca3948aa50adfc31466bd
Alex Bonder <lexbonder@gmail.com>
2023-08-15 08:52:23 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 58 - Using NSPredicate

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -5,12 +5,39 @@
 //  Created by Alex Bonder on 8/14/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    
     var body: some View {
-        Text("Hello World")
-            .padding()
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown Name")
+            }
+            
+            Button("Add to Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
+            }
+        }
     }
 }
 
