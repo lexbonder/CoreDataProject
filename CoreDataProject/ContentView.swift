90a5cd952975dd1cdfd1eba6cf5ffa4e8987e71d
Alex Bonder <lexbonder@gmail.com>
2023-08-15 15:09:56 -0400

    Hacking With Swift - 100 Days of SwiftUI - Day 58
    One to Many relationships, core data, swiftUI, fetch request, and MERGE POLICY

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -10,36 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @State private var lastNameFilter = "S"
+    @FetchRequest(sortDescriptors: []) var countries: FetchedResults<Country>
     
     var body: some View {
         VStack {
-            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
-                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            List {
+                ForEach(countries, id: \.self) { country in
+                    Section(country.wrappedFullName) {
+                        ForEach(country.candyArray, id: \.self) { candy in
+                            Text(candy.wrappedName)
+                        }
+                    }
+                }
             }
             
-            Button("Add to Examples") {
-                let taylor = Singer(context: moc)
-                taylor.firstName = "Taylor"
-                taylor.lastName = "Swift"
+            Button("Add Examples") {
+                let candy1 = Candy(context: moc)
+                candy1.name = "Mars"
+                candy1.origin = Country(context: moc)
+                candy1.origin?.shortName = "UK"
+                candy1.origin?.fullName = "United Kingdom"
                 
-                let bug = Singer(context: moc)
-                bug.firstName = "Bug"
-                bug.lastName = "Hunter"
+                let candy2 = Candy(context: moc)
+                candy2.name = "KitKat"
+                candy2.origin = Country(context: moc)
+                candy2.origin?.shortName = "UK"
+                candy2.origin?.fullName = "United Kingdom"
                 
-                let frank = Singer(context: moc)
-                frank.firstName = "Frank"
-                frank.lastName = "Turner"
+                let candy3 = Candy(context: moc)
+                candy3.name = "Twix"
+                candy3.origin = Country(context: moc)
+                candy3.origin?.shortName = "UK"
+                candy3.origin?.fullName = "United Kingdom"
+                
+                let candy4 = Candy(context: moc)
+                candy4.name = "Toblerone"
+                candy4.origin = Country(context: moc)
+                candy4.origin?.shortName = "CH"
+                candy4.origin?.fullName = "Switzerland"
                 
                 try? moc.save()
-            }
-            
-            Button("Show H") {
-                lastNameFilter = "H"
-            }
-            
-            Button("Show T") {
-                lastNameFilter = "T"
             }
         }
     }
